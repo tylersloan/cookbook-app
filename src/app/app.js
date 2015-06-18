3c226f8f86f733b0eb2a727c56186d1a752177f6
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-06-18 00:38:55 -0400

    put 'view' directories into /views

--- src/app/app.js
+++ src/app/app.js
@@ -5,19 +5,19 @@ var ngCookbook = angular.module('ngCookbook', ['ngRoute']);
 ngCookbook.config(function ($routeProvider) {
     $routeProvider
     .when('/', {
-        templateUrl: 'app/dashboard/dashboard.html',
+        templateUrl: 'app/views/dashboard/dashboard.html',
         controller: 'DashboardCtrl'
     })
     .when('/signin', {
-        templateUrl: 'app/signin/signin.html',
+        templateUrl: 'app/views/signin/signin.html',
         controller: 'SignInCtrl'
     })
     .when('/new-recipe', {
-        templateUrl: 'app/new-recipe/new-recipe.html',
+        templateUrl: 'app/views/new-recipe/new-recipe.html',
         controller: 'NewRecipeCtrl'
     })
-    .when('/recipe/', {
-        templateUrl: 'app/view-recipe/recipe.html',
+    .when('/recipe', {
+        templateUrl: 'app/views/view-recipe/recipe.html',
         controller: 'ViewRecipeCtrl'
     })
     .otherwise({
