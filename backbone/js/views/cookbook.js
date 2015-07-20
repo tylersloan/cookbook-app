a138c9282fadfbb8444c035ff774caee91aa33ab
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-07-20 06:40:03 -0400

    cookbook view progress

--- backbone/js/views/cookbook.js
+++ backbone/js/views/cookbook.js
@@ -1,3 +1,16 @@
-/**
- * Created by Tyler on 7/20/15.
- */
+app.CookbookView = Backbone.View.extend({
+	el: '#recipes',
+
+	initialize: function( initialRecipes ) {
+		this.collection = new app.Cookbook( initialRecipes );
+		this.render();
+	},
+
+	render: function() {
+		this.collection.each(function( item ) {
+			this.renderRecipe( item );
+		}, this)
+	}
+
+	// not finished
+})
