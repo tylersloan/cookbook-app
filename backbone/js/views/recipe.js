6fb487bff85428c9294af01ac7fee52bc7dc5d73
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-07-20 06:36:07 -0400

    recipe view

--- backbone/js/views/recipe.js
+++ backbone/js/views/recipe.js
@@ -1,3 +1,10 @@
-/**
- * Created by Tyler on 7/20/15.
- */
+app.RecipeView = Backbone.View.extend({
+	tagName: 'div',
+	className: 'recipe-view-classname',
+	template: _.template( $('#js-tmpl-view-recipe'.html()) ),
+
+	render: function() {
+		this.$el.html( this.template( this.model.attributes ) );
+		return this;
+	}
+})
