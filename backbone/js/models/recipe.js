3b372dc2282c735f24afe15a4be05f61c8f47e4c
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-07-20 06:26:28 -0400

    recipe model defaults

--- backbone/js/models/recipe.js
+++ backbone/js/models/recipe.js
@@ -1,3 +1,17 @@
-/**
- * Created by Tyler on 7/20/15.
- */
+app.Recipe = Backbone.Model.extend({
+	defaults: {
+		image: 'https://download.unsplash.com/photo-1437384423356-1aed23732c43',
+		name: 'No Recipe Name',
+		author: 'Unknown Author',
+		category: 'Unknown',
+		cook_time: '0',
+		prep_time: '0',
+		cook_temperature: '0',
+		yield_key: 'None',
+		yield_value: '0',
+		ingredient_amount: '0',
+		ingredient_name: 'None',
+		directions: 'None',
+		notes: 'None'
+	}
+});
