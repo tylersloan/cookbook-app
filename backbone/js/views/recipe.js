440ddc2c957ca881f172f31d9e1b09c07b196c11
Tyler Sloan <tyler.g.sloan@gmail.com>
2015-08-03 06:34:38 -0400

    fixed jquery typo in recipe.js

--- backbone/js/views/recipe.js
+++ backbone/js/views/recipe.js
@@ -1,7 +1,7 @@
 app.RecipeView = Backbone.View.extend({
 	tagName: 'div',
 	className: 'recipe-view-classname',
-	template: _.template( $('#js-tmpl-view-recipe'.html()) ),
+	template: _.template( $('#js-tmpl-view-recipe').html() ),
 
 	render: function() {
 		this.$el.html( this.template( this.model.attributes ) );
